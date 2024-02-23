853a060775a1d645b59326545795e237fd602ccb
Astro <astroorbis@gmail.com>
2024-02-23 15:43:59 -0500

    express starter

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// API server for custom frontends/webuis
+
+const express = require('express');
+
+class APIServer {
+	constructor(tradelogger) {
+		this.app = express();
+		this.tradelog = tradelogger;
+
+		this.app.use(express.json());
+
+		this.app.get('/log', (req, res) => {
+			res.send(this.tradelog.log);
+		});
+	}
+
+	start() {
+		this.app.listen(3333, () => {
+			console.log('API Server listening on port 3333');
+		});
+	}
+}
